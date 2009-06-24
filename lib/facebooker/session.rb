adbe50cb2c48584f078b1ac141360983363e39b2
ckhsponge <ckhobbs@gmail.com>
2009-06-24 15:31:41 -0700

    generate_session_secret is always 1 or 0

--- lib/facebooker/session.rb
+++ lib/facebooker/session.rb
@@ -183,12 +183,12 @@ module Facebooker
     end
 
     def secure!(args = {})
-      response = post 'facebook.auth.getSession', :auth_token => auth_token, :generate_session_secret => args[:generate_session_secret]
+      response = post 'facebook.auth.getSession', :auth_token => auth_token, :generate_session_secret => args[:generate_session_secret] ? "1" : "0"
       secure_with!(response['session_key'], response['uid'], response['expires'], response['secret'])
     end    
     
     def secure_with_session_secret!
-      self.secure!(:generate_session_secret => "1")
+      self.secure!(:generate_session_secret => true)
     end
 
     def secure_with!(session_key, uid = nil, expires = nil, secret_from_session = nil)
